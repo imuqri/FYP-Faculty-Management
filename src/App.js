39d6fdfe80b203f9fc5f4614751a8f8b5d111487
Slash Hj Tapah <imuqri31@gmail.com>
2023-10-09 01:29:40 +0800

    Add create user function
    basic user auth to firebase using email and pass, doesnt add user role yet

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Login from './components/Login';
 import AddUser from './components/AddUser';
 import Account from './components/Account';
+import { AuthContextProvider } from "./context/AuthContext";
 
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/addUser' element={<AddUser/>}/>
-        <Route path='/account' element={<Account/>}/>
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/addUser' element={<AddUser/>}/>
+          <Route path='/account' element={<Account/>}/>
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const AddUser = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  
+  const { createUser } = UserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try{
+      await createUser(email, password)
+    } catch (e) {
+      setError(e.message)
+      console.log(e.message)
+    }
+  }
+
   return (
     <div className='container'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Email Address</label>
-          <input type="email"/>
+          <input onChange={(e) => setEmail(e.target.value)} type="email"/>
         </div>
         <div>
           <label>Password</label>
-          <input type="password"/>
+          <input onChange={(e) =>setPassword(e.target.value)} type="password"/>
         </div>
         <button>Add User</button>
       </form>
-      <p>Log in Comp <Link to='/'>Add User</Link></p>
+      <p>Log in Comp <Link to='/'>Log In</Link></p>
     </div>
   )
 }
