0ecadcf91e7184d029ca07596da5b0eab322c7a1
Slash Hj Tapah <imuqri31@gmail.com>
2023-10-08 20:42:57 +0800

    add route to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from './components/Login';
+import AddUser from './components/AddUser';
+import Account from './components/Account';
+
+
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/addUser' element={<AddUser/>}/>
+        <Route path='/account' element={<Account/>}/>
+      </Routes>
     </div>
   );
 }
