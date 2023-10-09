b4e0d48c238dce8ab28e860bbe49390ef73ba976
Slash Hj Tapah <imuqri31@gmail.com>
2023-10-09 22:19:54 +0800

    Update Account.jsx
    fetch user role from db and display on account information

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { UserAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import { ref, get } from 'firebase/database';
+import { database } from '../firebase';
 
 const Account = () => {
 
   const{user, logout} = UserAuth()
+  const [userRole, setUserRole] = useState();
   const navigate = useNavigate() 
+
+  useEffect(() => {
+    // Fetch the user role when the component mounts
+    const fetchUserRole = async () => {
+      try {
+        if (user && user.uid) {
+          const userRef = ref(database, `users/${user.uid}`);
+          const snapshot = await get(userRef);
+          if (snapshot.exists()) {
+            setUserRole(snapshot.val().role);
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching user role:', error);
+      }
+    };
+
+    fetchUserRole();
+  }, [user]); // Fetch the user role whenever the user object changes
 
   const handleLogout = async () => {
     try {
@@ -20,8 +42,8 @@ const Account = () => {
   return (
     <div>
       <h1>account</h1>
-      <p>User Email:{user && user.email}</p>
-      <p>User Role:</p>
+      <p>User Email: {user && user.email}</p>
+      <p>User Role: {userRole}</p>
 
       <button onClick={handleLogout}>Logout</button>
     </div>
