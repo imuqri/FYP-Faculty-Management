c9e01ab2490aef0b36a11ce6764e27e4d2fd5dac
Slash Hj Tapah <imuqri31@gmail.com>
2023-11-19 23:27:01 +0800

    update login page
    create UI for login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import AddUser from "./components/AddUser";
 import Account from "./components/Account";
 import { AuthContextProvider } from "./context/AuthContext";
