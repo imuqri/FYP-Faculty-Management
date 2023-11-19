edce796c2eb0b017c9d96069d85f6f760ec43a1c
Slash Hj Tapah <imuqri31@gmail.com>
2023-11-19 23:35:52 +0800

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 
-const PrivateRoute = ( {children}) => {
-    const {user} = UserAuth()
+const PrivateRoute = ({ children }) => {
+  const { user } = UserAuth();
 
-    if(!user) {
-        return <Navigate to='/'/>
-    }
-
-  return children
-}
+  return user ? (
+    // Render the children if the user is authenticated
+    children
+  ) : (
+    // Redirect to the login page if the user is not authenticated
+    <Navigate to="/" replace={true} />
+  );
+};
 
 export default PrivateRoute
