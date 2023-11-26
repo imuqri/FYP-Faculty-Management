3b79a1e4c578aadb8bb0517c79513536f11cb6ac
Slash Hj Tapah <imuqri31@gmail.com>
2023-11-26 18:47:31 +0800

    create add version on SoftwareList comp
    adjust the styling for SoftwareList all functioning

--- src/components/SoftwareList.jsx
+++ src/components/SoftwareList.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { ref, onValue, remove, update } from 'firebase/database';
 import { database } from '../firebase';
-import { Accordion, Button, Modal, Form, ListGroup, Card } from 'react-bootstrap';
+import { Button, Modal, Form, ListGroup, Card } from 'react-bootstrap';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+import { MdOutlineDeleteForever,MdOutlineEditNote } from "react-icons/md";
 
 const SoftwareList = () => {
   const [softwareList, setSoftwareList] = useState([]);
@@ -11,6 +13,9 @@ const SoftwareList = () => {
   const [showEditVersionModal, setShowEditVersionModal] = useState(false);
   const [editVersionIndex, setEditVersionIndex] = useState(null);
   const [newVersion, setNewVersion] = useState('');
+  const [showAddVersionModal, setShowAddVersionModal] = useState(false);
+  const [newVersionInput, setNewVersionInput] = useState('');
+  const [openSoftware, setOpenSoftware] = useState(null);
 
   useEffect(() => {
     const softwareRef = ref(database, 'softwares');
@@ -68,7 +73,6 @@ const SoftwareList = () => {
   const handleEditVersion = () => {
     const updatedVersions = [...softwareList[editSoftwareId].versions];
     updatedVersions[editVersionIndex] = newVersion;
-
     const softwareRef = ref(database, `softwares/${softwareList[editSoftwareId].id}`);
     update(softwareRef, { versions: updatedVersions })
       .then(() => {
@@ -83,7 +87,6 @@ const SoftwareList = () => {
   const handleDeleteVersion = (softwareIndex, versionIndex) => {
     const updatedVersions = [...softwareList[softwareIndex].versions];
     updatedVersions.splice(versionIndex, 1);
-
     const softwareRef = ref(database, `softwares/${softwareList[softwareIndex].id}`);
     update(softwareRef, { versions: updatedVersions })
       .then(() => {
@@ -94,43 +97,82 @@ const SoftwareList = () => {
       });
   };
 
+  const handleShowAddVersionModal = (software) => {
+    setShowAddVersionModal(true);
+    setEditSoftwareId(software.id);
+  };
+
+  const handleAddVersion = () => {
+    const currentSoftware = softwareList.find((software) => software.id === editSoftwareId);
+
+    if (currentSoftware && currentSoftware.versions !== undefined) {
+      const updatedVersions = [...currentSoftware.versions, newVersionInput];
+
+      const softwareRef = ref(database, `softwares/${editSoftwareId}`);
+      update(softwareRef, { versions: updatedVersions })
+        .then(() => {
+          console.log('Version added successfully.');
+          setShowAddVersionModal(false);
+          setNewVersionInput('');
+        })
+        .catch((error) => {
+          console.error('Error adding version:', error);
+        });
+    } else {
+      console.error('Invalid software or versions data:', editSoftwareId, softwareList);
+    }
+  };
+
+  const toggleSoftware = (index) => {
+    setOpenSoftware(openSoftware === index ? null : index);
+  };
+
   return (
     <div className="container mt-5">
       <h2>Software List</h2>
       {softwareList.map((software, softwareIndex) => (
         <Card key={software.id} className="mb-3">
-          <Accordion defaultActiveKey="0">
-            <Accordion.Item eventKey="0">
-              <Card.Header className="d-flex justify-content-between align-items-center">
-                {software.name}
-                <div>
-                  <Button variant="primary" className="me-2" onClick={() => handleShowEditModal(software.id)}>
-                    Edit
-                  </Button>
-                  <Button variant="danger" onClick={() => handleDeleteSoftware(software.id)}>
-                    Delete
-                  </Button>
-                </div>
-              </Card.Header>
-              <Accordion.Body>
-                <ListGroup>
-                  {software.versions.map((version, versionIndex) => (
-                    <ListGroup.Item key={versionIndex} className="d-flex justify-content-between align-items-center">
-                      {version}
-                      <div>
-                        <Button variant="primary" className="me-2" onClick={() => handleShowEditVersionModal(softwareIndex, versionIndex)}>
-                          Edit
-                        </Button>
-                        <Button variant="danger" onClick={() => handleDeleteVersion(softwareIndex, versionIndex)}>
-                          Delete
-                        </Button>
-                      </div>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
+          <Card.Header className="d-flex justify-content-between align-items-center">
+            <div onClick={() => toggleSoftware(softwareIndex)} style={{ cursor: 'pointer' }}>
+              {openSoftware === softwareIndex ? <BsChevronUp /> : <BsChevronDown />}  
+              {software.name}
+
+            </div>
+            <div>
+              <Button variant="primary" className="me-2" onClick={() => handleShowEditModal(software.id)}>
+                <MdOutlineEditNote />
+              </Button>
+              <Button variant="danger" onClick={() => handleDeleteSoftware(software.id)}>
+                <MdOutlineDeleteForever />
+              </Button>
+            </div>
+          </Card.Header>
+          {openSoftware === softwareIndex && (
+            <Card.Body>
+              <ListGroup>
+                {software.versions.map((version, versionIndex) => (
+                  <ListGroup.Item key={versionIndex} className="d-flex justify-content-between align-items-center">
+                    {version}
+                    <div>
+                      <Button
+                        variant="primary"
+                        className="me-2"
+                        onClick={() => handleShowEditVersionModal(softwareIndex, versionIndex)}
+                      >
+                        <MdOutlineEditNote />
+                      </Button>
+                      <Button variant="danger" onClick={() => handleDeleteVersion(softwareIndex, versionIndex)}>
+                        <MdOutlineDeleteForever />
+                      </Button>
+                    </div>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+              <Button variant="success" className="mt-3" onClick={() => handleShowAddVersionModal(software)}>
+                Add Version
+              </Button>
+            </Card.Body>
+          )}
         </Card>
       ))}
 
@@ -176,6 +218,29 @@ const SoftwareList = () => {
           </Button>
           <Button variant="primary" onClick={handleEditVersion}>
             Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Add Version Modal */}
+      <Modal show={showAddVersionModal} onHide={() => setShowAddVersionModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Version</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control
+            type="text"
+            placeholder="Enter new version"
+            value={newVersionInput}
+            onChange={(e) => setNewVersionInput(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowAddVersionModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAddVersion}>
+            Add Version
           </Button>
         </Modal.Footer>
       </Modal>
