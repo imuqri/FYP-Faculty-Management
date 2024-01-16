4121d912af2b6384ef022f9b6e35dfe4c07651c5
Slash Hj Tapah <imuqri31@gmail.com>
2024-01-16 15:37:05 +0800

    Update ManageLab.jsx

--- src/components/ManageLab.jsx
+++ src/components/ManageLab.jsx
@@ -21,23 +21,6 @@ const ManageLab = () => {
   const [selectedLab, setSelectedLab] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
-  const uniqueEquipments = Array.from(
-    new Set(labs.flatMap((lab) => lab.equipments || []))
-  );
-
-  const uniqueSoftwares = Array.from(
-    new Set(
-      labs.flatMap((lab) =>
-        (lab.softwares || []).map((software) => software.name)
-      )
-    )
-  );
-
-  // New state variables for filters
-  const [capacityFilter, setCapacityFilter] = useState("all"); // "all", "lt30", "31-40", "gt40"
-  const [equipmentFilter, setEquipmentFilter] = useState("all");
-  const [softwareFilter, setSoftwareFilter] = useState("all");
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -72,37 +55,6 @@ const ManageLab = () => {
     setSelectedLab(null);
   };
 
-  const applyFilters = (lab) => {
-    // Capacity filter
-    const isCapacityMatch =
-      (capacityFilter === "lt30" && lab.capacity < 30) ||
-      (capacityFilter === "31-40" &&
-        lab.capacity >= 31 &&
-        lab.capacity <= 40) ||
-      (capacityFilter === "gt40" && lab.capacity > 40) ||
-      capacityFilter === "all";
-
-    // Equipment filter
-    const isEquipmentMatch =
-      equipmentFilter === "all" ||
-      (lab.equipments && lab.equipments.includes(equipmentFilter));
-
-    // Software filter
-    const isSoftwareMatch =
-      softwareFilter === "all" ||
-      (lab.softwares &&
-        lab.softwares.some((software) => software.name === softwareFilter));
-
-    return isCapacityMatch && isEquipmentMatch && isSoftwareMatch;
-  };
-
-  const capacityOptions = [
-    { value: "all", label: "All Capacities" },
-    { value: "lt30", label: "< 30" },
-    { value: "31-40", label: "31-40" },
-    { value: "gt40", label: "> 40" },
-  ];
-
   return (
     <Container className="mt-3 mb-3">
       <Form className="mb-3">
@@ -114,61 +66,10 @@ const ManageLab = () => {
         />
       </Form>
 
-      {/* Filter options */}
-      <Form className="mb-3">
-        <Row>
-          {/* Capacity filter */}
-          <Col md={4} className="mb-3">
-            <Form.Select
-              onChange={(e) => setCapacityFilter(e.target.value)}
-              value={capacityFilter}
-            >
-              {capacityOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </Form.Select>
-          </Col>
-
-          {/* Equipment filter */}
-          <Col md={4} className="mb-3">
-            <Form.Select
-              onChange={(e) => setEquipmentFilter(e.target.value)}
-              value={equipmentFilter}
-            >
-              <option value="all">All Equipments</option>
-              {uniqueEquipments.map((equipment, index) => (
-                <option key={index} value={equipment}>
-                  {equipment}
-                </option>
-              ))}
-            </Form.Select>
-          </Col>
-
-          {/* Software filter */}
-          <Col md={4} className="mb-3">
-            <Form.Select
-              onChange={(e) => setSoftwareFilter(e.target.value)}
-              value={softwareFilter}
-            >
-              <option value="all">All Softwares</option>
-              {uniqueSoftwares.map((software, index) => (
-                <option key={index} value={software}>
-                  {software}
-                </option>
-              ))}
-            </Form.Select>
-          </Col>
-        </Row>
-      </Form>
-
       <Row xs={1} md={2} lg={4} xl={4} className="g-4">
         {labs
-          .filter(
-            (lab) =>
-              lab.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
-              applyFilters(lab)
+          .filter((lab) =>
+            lab.name.toLowerCase().includes(searchTerm.toLowerCase())
           )
           .map((lab) => (
             <Col key={lab.id}>
